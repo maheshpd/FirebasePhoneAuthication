3f94ab536252205448b9a0b67b1ab8b6244271b7
Dharmesh Rathod <>
2019-10-04 10:35:54 +0530

    initial commit

---
+++ app/src/main/java/com/example/firebasemobileauthicationdemo/MainActivity.java
@@ -0,0 +1,177 @@
+package com.example.firebasemobileauthicationdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final String KEY_VERIFY_IN_PROGRESS = "key_verify_in_progress";
+
+    private static final int STATE_INITIALIZED = 1;
+    private static final int STATE_CODE_SENT = 2;
+    private static final int STATE_VERIFY_FAILED = 3;
+    private static final int STATE_VERIFY_SUCCESS = 4;
+    private static final int STATE_SIGNIN_FAILED = 5;
+    private static final int STATE_SIGNIN_SUCCESS = 6;
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    private boolean mVerificationInProgress = false;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
+    private ViewGroup mPhoneNumberViews;
+    private ViewGroup mSignedInViews;
+
+    private TextView mStatusText;
+    private TextView mDetailText;
+
+    private EditText mPhoneNumberField;
+    private EditText mVerificationField;
+
+    private Button mStartButton;
+    private Button mVerifyButton;
+    private Button mResendButton;
+    private Button mSignOutButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Restore instance state
+        if (savedInstanceState != null) {
+            onRestoreInstanceState(savedInstanceState);
+        }
+
+        // Assign views
+        mPhoneNumberViews = findViewById(R.id.phoneAuthFields);
+        mSignedInViews = findViewById(R.id.signedInButtons);
+
+        mStatusText = findViewById(R.id.status);
+        mDetailText = findViewById(R.id.detail);
+
+        mPhoneNumberField = findViewById(R.id.fieldPhoneNumber);
+        mVerificationField = findViewById(R.id.fieldVerificationCode);
+
+        mStartButton = findViewById(R.id.buttonStartVerification);
+        mVerifyButton = findViewById(R.id.buttonVerifyPhone);
+        mResendButton = findViewById(R.id.buttonResend);
+        mSignOutButton = findViewById(R.id.signOutButton);
+
+        // Assign click listeners
+        mStartButton.setOnClickListener(this);
+        mVerifyButton.setOnClickListener(this);
+        mResendButton.setOnClickListener(this);
+        mSignOutButton.setOnClickListener(this);
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        // Initialize phone auth callbacks
+        // [START phone_auth_callbacks]
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+
+                Log.d(TAG, "onVerificationCompleted: " + phoneAuthCredential);
+                mVerificationInProgress = false;
+
+                updateUI(STATE_VERIFY_SUCCESS,phoneAuthCredential);
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                Log.w(TAG, "onVerificationFailed", e);
+
+                mVerificationInProgress = false;
+
+                if (e instanceof FirebaseAuthInvalidCredentialsException){
+                    // Invalid request
+                    mPhoneNumberField.setError("Invalid phone Number");
+                } else if (e instanceof FirebaseTooManyRequestsException){
+                    Snackbar.make(findViewById(android.R.id.content),"Quota exceeded.",Snackbar.LENGTH_SHORT).show();
+                }
+
+                updateUI(STATE_SIGNIN_FAILED);
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+
+                Log.d(TAG, "onCodeSent:" + verificationId);
+
+                // Save verification ID and resending token so we can use them later
+
+                mVerificationId = verificationId;
+                mResendToken = token;
+
+                updateUI(STATE_CODE_SENT);
+
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+
+        // [START_EXCLUDE]
+        if (mVerificationInProgress && validatePhoneNumber()) {
+            startPhoneNumberVerification(mPhoneNumberField.getText().toString());
+        }
+        // [END_EXCLUDE]
+
+    }
+
+    private void startPhoneNumberVerification(String toString) {
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_VERIFY_IN_PROGRESS, mVerificationInProgress);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mVerificationInProgress = savedInstanceState.getBoolean(KEY_VERIFY_IN_PROGRESS);
+    }
+
+}
